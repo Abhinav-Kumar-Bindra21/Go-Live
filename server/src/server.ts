430a3ab4cbc950f9cd897c73163257cbcdb1a3bf
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-09-26 10:17:15 +0530

    connected to neon databse

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,21 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { initDb } from "./config/db";
 
+const app = express();
+
+// Connect to Neon & Initialize Tables
+initDb();
+
+const PORT = process.env.PORT || 3000;
+
+const allowedOrigins = process.env.ORIGINS?.split(",");
+app.use(cors({ origin: "", credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server Started");
+});
